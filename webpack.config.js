dc61f7ae3c905abcce6cb73eefa2f0ea46c1cc46
Luis Carlin <luiscarlin@gmail.com>
2018-11-12 07:42:48 -0500

    add more config

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,32 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: [
+      './src/index.js'
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: ['babel-loader']
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['*', '.js', '.jsx']
+    },
+    output: {
+      path: __dirname + '/dist',
+      publicPath: '/100-days-of-css/',
+      filename: 'bundle.js'
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './public/index.html'
+      })
+    ],
+    devServer: {
+      contentBase: './dist'
+    }
+  };
