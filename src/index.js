fc42650e9d3e9fea010f7e24353ab5be27cd50eb
Luis Carlin <luiscarlin@gmail.com>
2018-11-14 06:16:07 -0500

    finish setup

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const rootElement = document.getElementById('root')
+
+const App = () => (<div>Hello world!</div>)
+
+ReactDOM.render(
+  <App />,
+  rootElement
+)
