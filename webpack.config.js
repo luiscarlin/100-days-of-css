132845ef300bf464efa7fc927952f4ae0730e7ba
Luis Carlin <luiscarlin@gmail.com>
2018-11-14 05:57:03 -0500

    formatting

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,34 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: [
-      './src/index.js'
-    ],
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader"
-          }
+  entry: [
+    './src/index.js'
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
         }
-      ]
-    },
-    resolve: {
-      extensions: ['*', '.js', '.jsx']
-    },
-    output: {
-      path: __dirname + '/dist',
-      publicPath: '/100-days-of-css/',
-      filename: 'bundle.js'
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: './public/index.html'
-      })
-    ],
-    devServer: {
-      contentBase: './dist'
-    }
-  };
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/100-days-of-css/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    })
+  ],
+  devServer: {
+    contentBase: './dist'
+  }
+};
