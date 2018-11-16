866ad55e96bc7366905dd44680bf1fd899f34faf
Luis Carlin <luiscarlin@gmail.com>
2018-11-16 05:49:20 -0500

    Add hover effect on dot

--- src/components/challengeN49/script.js
+++ src/components/challengeN49/script.js
@@ -3,7 +3,7 @@ $(function() {
 	const numberOfCells = 400
 
 	for(let i = 0; i < numberOfCells; i++) {
-		$(".frame").append('<div class="cell"></div>')
+		$(".frame").append('<div class="cell cell-' + i + '"></div>')
 	}
 
 	$(".frame").append('<div class="dot"></div>')
