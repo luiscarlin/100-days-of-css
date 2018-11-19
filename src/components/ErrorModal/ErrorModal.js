891c84e285c4d410f2f1a446f52a93dda39407ab
Luis Carlin <luiscarlin@gmail.com>
2018-11-19 06:48:58 -0500

    Clean up error modal

--- src/components/ErrorModal/ErrorModal.js
+++ src/components/ErrorModal/ErrorModal.js
@@ -1,29 +1,28 @@
 import React from 'react'
 import './error-modal.scss'
 
-// $(document).ready(function () {
-//   setTimeout(function () {
-//     $(".modal").removeClass("out");
-//   }, 500);
-//   $("button").click(function () {
-//     $(this).parent().addClass("out").css("transition", "all 500ms cubic-bezier(0.50, -0.25, 0.60, 0.75) 0s");
-//   })
-// });
-
 export default class ErrorModal extends React.Component {
-  // componentDidMount() {
+  constructor(props) {
+    super(props)
 
-  // }
+    this.state = {
+      hide: true
+    }
+  }
+
+  componentDidMount() {
+   setTimeout(() => this.setState({ hide: false }), 500)
+  }
 
   render() {
     return (
       <div className="frame">
         <div className="center">
-          <div className="modal out">
+          <div className={this.state.hide ? "modal out" : "modal" }>
             <img src="http://100dayscss.com/codepen/alert.png" alt="Error" />
             <label>Oh Snap!</label>
             <p>An error has occured while creating an error report.</p>
-            <button>Dismiss</button>
+            <button onClick={() => this.setState({ hide: true })}>Dismiss</button>
           </div>
         </div>
       </div>
